d117180294b5b774cceef716873e2c20c7c369a3
BerkeKalkan <berkekalkantr@gmail.com>
2021-01-16 13:24:25 +0300

    Configure localization

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:order_tracking/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:order_tracking/models/user.dart';
+//import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +17,21 @@ class MyApp extends StatelessWidget {
     return StreamProvider<User>.value(
       value: AuthService().user,
       child: MaterialApp(
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('tr', ''), // Turkish
+          const Locale('en', ''), // English
+        ],
+        localeResolutionCallback: (locale, supportedLocales) {
+          for (var supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale.languageCode) {
+              return supportedLocale;
+            }
+          }
+          return supportedLocales.first;
+        },
         home: Wrapper(),
       ),
     );
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:order_tracking/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:order_tracking/shared/constants.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
