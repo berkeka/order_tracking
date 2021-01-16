c12de6195ae45ff2a2137a920da36bb7c4f9d56d
doguk <dogukan.hacifazlioglu@bil.omu.edu.tr>
2021-01-16 15:26:50 +0300

    Add product picture edit option

--- lib/screens/products/product_edit.dart
+++ lib/screens/products/product_edit.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:order_tracking/models/product.dart';
 import 'package:order_tracking/shared/constants.dart';
 import 'package:order_tracking/services/database.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProductEdit extends StatefulWidget {
   final Product product;
@@ -13,6 +16,62 @@ class ProductEdit extends StatefulWidget {
 class _ProductEditState extends State<ProductEdit> {
   DatabaseService _databaseService = DatabaseService();
   final _formKey = GlobalKey<FormState>();
+
+  //For image picker
+  File _image;
+  final _picker = ImagePicker();
+
+  _imgFromCamera() async {
+    PickedFile image = await _picker.getImage(
+      source: ImageSource.camera, imageQuality: 50
+    );
+
+    setState(() {
+      _image = File(image.path);
+    });
+  }
+
+  _imgFromGallery() async {
+    PickedFile image = await  _picker.getImage(
+        source: ImageSource.gallery, imageQuality: 50
+    );
+
+    setState(() {
+      _image = File(image.path);      
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Photo Library'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      );
+  }
+  //For image picker
 
   String error = '';
   String name = '';
@@ -73,6 +132,56 @@ class _ProductEditState extends State<ProductEdit> {
               Text('Description'),
               TextFormField(
                 controller: descriptionController,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 40.0),
+                child: Text("Picture"),
+              ),
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                  ),
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        _showPicker(context);
+                      },
+                      child: Container(
+                        child: _image != null
+                            ? Container(
+                                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                                child : ClipRRect(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(8.0),
+                                        topRight: Radius.circular(8.0),
+                                      ),
+                                      child: Image.file(
+                                          _image,
+                                          // width: 300,
+                                          height: 150,
+                                          fit:BoxFit.fill
+                                      ),
+                                    ),
+                              )
+                            : Container(
+                                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                                child : ClipRRect(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(8.0),
+                                        topRight: Radius.circular(8.0),
+                                      ),
+                                      child: Image.network(
+                                          '${product.imageURL}',
+                                          // width: 300,
+                                          height: 150,
+                                          fit:BoxFit.fill
+                                      ),
+                                    ),
+                              ),
+                      ),
+                    ),
+                  )
+                ],
               ),
               SizedBox(height: 10.0),
               RaisedButton(
