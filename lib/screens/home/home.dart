6d37deb8201ad57341eee67d31897750b3f6d7ee
BerkeKalkan <berkekalkantr@gmail.com>
2021-01-16 19:04:42 +0300

    Fix minor issues

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -110,6 +110,7 @@ class _HomeState extends State<Home> {
                           userLocation.latitude,
                           userLocation.longitude,
                         ),
+                        hasorder: false,
                       ),
                     );
                   }
